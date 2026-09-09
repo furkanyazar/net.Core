309be31a2f9f8ad5de862d05512e9daf48b934ae
Furkan YAZAR <furkanyazar@proton.me>
2026-09-09 17:48:34 +0300

    feat: add translation

---
+++ src/Core.Translation/AmazonTranslate/AmazonTranslateConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Amazon;
+
+namespace Core.Translation.AmazonTranslate;
+
+public class AmazonTranslateConfiguration(
+    string accessKey,
+    string secretKey,
+    RegionEndpoint regionEndpoint
+)
+{
+    public string AccessKey { get; } = accessKey;
+    public string SecretKey { get; } = secretKey;
+    public RegionEndpoint RegionEndpoint { get; } = regionEndpoint;
+}
