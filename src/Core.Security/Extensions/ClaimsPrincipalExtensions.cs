afdb87d8e23c7a888aa36021b875fadae75ec3a8
Furkan YAZAR <furkanyazar@proton.me>
2026-09-09 18:39:54 +0300

    feat: add security

---
+++ src/Core.Security/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using System.Security.Claims;
+
+namespace Core.Security.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static ICollection<string>? GetClaims(
+        this ClaimsPrincipal claimsPrincipal,
+        string claimType
+    )
+    {
+        return claimsPrincipal?.FindAll(claimType)?.Select(x => x.Value).ToImmutableArray();
+    }
+
+    public static ICollection<string>? GetRoleClaims(this ClaimsPrincipal claimsPrincipal)
+    {
+        return claimsPrincipal?.GetClaims(ClaimTypes.Role);
+    }
+
+    public static string? GetIdClaim(this ClaimsPrincipal claimsPrincipal)
+    {
+        return claimsPrincipal?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    }
+}
---
+++ src/Core.Security/JWT/JwtHelper.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Immutable;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Core.Security.Encryption;
+using Core.Security.Entities;
+using Core.Security.Extensions;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Core.Security.JWT;
+
+public class JwtHelper<TUserId, TOperationClaimId, TRefreshTokenId>(TokenOptions tokenOptions)
+    : ITokenHelper<TUserId, TOperationClaimId, TRefreshTokenId>
+{
+    private readonly TokenOptions _tokenOptions = tokenOptions;
+
+    public virtual AccessToken CreateToken(
+        User<TUserId> user,
+        IList<OperationClaim<TOperationClaimId>> operationClaims
+    )
+    {
+        DateTime accessTokenExpiration = DateTime.Now.AddMinutes(
+            _tokenOptions.AccessTokenExpiration
+        );
+        SecurityKey securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+        SigningCredentials signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(
+            securityKey
+        );
+        JwtSecurityToken jwt = CreateJwtSecurityToken(
+            _tokenOptions,
+            user,
+            signingCredentials,
+            operationClaims,
+            accessTokenExpiration
+        );
+        JwtSecurityTokenHandler jwtSecurityTokenHandler = new();
+        string? token = jwtSecurityTokenHandler.WriteToken(jwt);
+
+        return new AccessToken() { Token = token, ExpirationDate = accessTokenExpiration };
+    }
+
+    public RefreshToken<TRefreshTokenId, TUserId> CreateRefreshToken(
+        User<TUserId> user,
+        string ipAddress
+    )
+    {
+        return new RefreshToken<TRefreshTokenId, TUserId>()
+        {
+            UserId = user.Id,
+            Token = RandomRefreshToken(),
+            ExpirationDate = DateTime.UtcNow.AddDays(_tokenOptions.RefreshTokenTTL),
+            CreatedByIp = ipAddress,
+        };
+    }
+
+    public virtual JwtSecurityToken CreateJwtSecurityToken(
+        TokenOptions tokenOptions,
+        User<TUserId> user,
+        SigningCredentials signingCredentials,
+        IList<OperationClaim<TOperationClaimId>> operationClaims,
+        DateTime accessTokenExpiration
+    )
+    {
+        return new JwtSecurityToken(
+            tokenOptions.Issuer,
+            tokenOptions.Audience,
+            expires: accessTokenExpiration,
+            notBefore: DateTime.Now,
+            claims: SetClaims(user, operationClaims),
+            signingCredentials: signingCredentials
+        );
+    }
+
+    protected virtual IEnumerable<Claim> SetClaims(
+        User<TUserId> user,
+        IList<OperationClaim<TOperationClaimId>> operationClaims
+    )
+    {
+        List<Claim> claims = [];
+        claims.AddNameIdentifier(user!.Id!.ToString()!);
+        claims.AddEmail(user.Email);
+        claims.AddRoles([.. operationClaims.Select(c => c.Name)]);
+        return [.. claims];
+    }
+
+    private string RandomRefreshToken()
+    {
+        byte[] numberByte = new byte[32];
+        using var random = RandomNumberGenerator.Create();
+        random.GetBytes(numberByte);
+        return Convert.ToBase64String(numberByte);
+    }
+}
