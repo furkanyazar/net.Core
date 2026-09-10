0926f079332e57442d7be02caec38c8aef15509d
Furkan YAZAR <furkanyazar@proton.me>
2026-09-10 16:04:16 +0300

    feat: add redis options

---
+++ src/Core.Application/Pipelines/Caching/RedisOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace Core.Application.Pipelines.Caching;
+
+public class RedisOptions
+{
+    public string Configuration { get; set; }
+
+    public RedisOptions()
+    {
+        Configuration = string.Empty;
+    }
+
+    public RedisOptions(string configuration)
+    {
+        Configuration = configuration;
+    }
+}
