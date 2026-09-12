337d8ade490808feb414f6f66a9a124234bb3b64
Furkan YAZAR <furkanyazar@proton.me>
2026-09-12 03:21:34 +0300

    refactor: remove unused classes

--- src/Core.Test/Application/Constants/ValidationErrorCodes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Core.Test.Application.Constants;
-
-public static class ValidationErrorCodes
-{
-    public static string NotEmptyValidator => "NotEmptyValidator";
-    public static string MinimumLengthValidator => "MinimumLengthValidator";
-    public static string EmailValidator => "EmailValidator";
-}
